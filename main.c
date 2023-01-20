946096e2bad28690f1378f6b271cb8f77e4a63c3
lukehayes <lukepurplehayes@gmail.com>
2023-01-20 18:29:24 +0000

    Test resulted in cool 3D rotation effect on 2D plane.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 float delta = 0.0;
 float c = 0.0;
+float N = 100.0;
 
 int main() {
 
@@ -18,7 +19,14 @@ int main() {
 
         BeginDrawing();
             ClearBackground(DARKGRAY);
-            DrawRectangle(1280/2 - 40, 320 + (sin(c) * 100.0), 80, 80, LIGHTGRAY);
+
+            for(int i = 0; i <= 100; i++)
+            {
+
+                /*DrawRectangle(100.0f * cos(i), 320 + (sin(c) * 100.0), 10, 10, LIGHTGRAY);*/
+                DrawRectangle(640 + cos(i) * sin(c) * N , 360 + -sin(i) * cos(c) * N, 10,10, LIGHTGRAY);
+            }
+            /*DrawRectangle(1280/2 - 40, 320 + (sin(c) * 100.0), 80, 80, LIGHTGRAY);*/
         EndDrawing();
 
     }
